8746e4f28dd12973da426aa0c1d634c5035bf3f1
Gordon Huang <dqna64@gmail.com>
2021-12-13 18:14:07 +1100

    create and add mesh to scene

--- mdn-threejs-demo/main.js
+++ mdn-threejs-demo/main.js
@@ -15,15 +15,28 @@ const scene = new THREE.Scene();
 // Create a camera. Placed in the scene as a rendering perspective.
 // Types include PerspectiveCamera and OrthographicCamera
 // Default FOV (first arg): 50
-const camera = newTHREE.PerspectiveCamera(75, WIDTH/HEIGHT, 1, 1000);
+const camera = new THREE.PerspectiveCamera(70, WIDTH/HEIGHT, 1, 1000);
 // Camera position values in 3D relative to origin. Unitless.
 // Default camera position x, y, z: 0
-camera.position.z = 60;
+camera.position.z = 30;
 scene.add(camera);
+
+let t = 0;
 
 function render() {
     requestAnimationFrame(render);
+    cube.rotation.set(t * Math.PI/6, t * Math.PI/6, 0);
+    t += 0.01
     renderer.render(scene, camera);
 }
+
+// Create a geometry: a 3D shape
+const boxGeometry = new THREE.BoxGeometry(10, 10, 10);
+// Create a material: a surface appearance e.g. color, texture etc.
+var basicMaterial = new THREE.MeshBasicMaterial({color: 0xd173f0})
+// Bring the geometry and material together in a mesh that
+// can then be placed in the scene.
+var cube = new THREE.Mesh(boxGeometry, basicMaterial)
+scene.add(cube);
 
 render();
