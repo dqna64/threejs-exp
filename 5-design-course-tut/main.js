4d050cbc2ac76b032096bb5a654ae6d80d2ba415
Gordon Huang <dqna64@gmail.com>
2021-12-25 04:45:55 +1100

    fix object instantiations

--- 5-design-course-tut/main.js
+++ 5-design-course-tut/main.js
@@ -19,12 +19,12 @@ const sizes = {
 
 const canvas = document.getElementById('drawingArea');
 
-const scene = THREE.Scene();
+const scene = new THREE.Scene();
 
-const camera = THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
-camera.position.z = 8
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
+camera.position.z = 8;
 
-const renderer = THREE.WebGLRenderer({canvas: canvas, antialias: true});
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
